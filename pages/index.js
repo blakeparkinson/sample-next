cb7cc241b6a91307ccf177b1147410fbf2047ed1
Blake Parkinson <bparkinson@twochairs.com>
2023-02-16 12:45:33 -0800

    testing

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
@@ -8,11 +8,13 @@ export default function Home() {
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      
+
       <main className={styles.main}>
-        <h1 className={styles.title}>Our Great Next App
+        <h1 className={styles.title}>
+          This is our test app.
           <div>
-            <a href="/server-side">SSR example</a> | <a href="/ssg">SSG example</a>
+            <a href="/server-side">SSR example</a> |{" "}
+            <a href="/ssg">SSG example</a>
           </div>
           <div>
             <a href="/dynamic-loader">Dynamic loader example</a>
@@ -20,8 +22,7 @@ export default function Home() {
         </h1>
       </main>
 
-      <footer className={styles.footer}>
-      </footer>
+      <footer className={styles.footer}></footer>
     </div>
-  )
+  );
 }
